ba66e62c9c922c34bc9a70f5d0df89de0862286e
vood <balitsky_igor@list.ru>
2022-09-17 11:42:09 +0300

    a

--- src/main/kotlin/ru/vood/redisdemo/kafka/SomeData.kt
+++ src/main/kotlin/ru/vood/redisdemo/kafka/SomeData.kt
@@ -1,12 +1,14 @@
 package ru.vood.redisdemo.kafka
 
+import com.fasterxml.jackson.annotation.JsonFormat
 import java.math.BigDecimal
-import java.util.Date
+import java.util.*
 
 data class SomeData(
     val id: Long,
     val string: String = "SomeString",
     val boolean: Boolean = false,
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-mm-dd HH:mi:ss")
     val date: Date = Date(),
     val int: Int = 12,
     val bigDecimal: BigDecimal = BigDecimal(12),
